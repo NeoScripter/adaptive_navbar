e6acffbd9ac63319f1412af9aafa76bd80a2bb7f
Ilya <sange0337@gmail.com>
2024-07-09 14:46:48 +0800

    set the color scheme programmically

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const header = document.querySelector('.header__navbar-wrapper');
-    document.querySelector('.burger__menu').addEventListener('click', function() {
-        this.classList.toggle('active');
+    const burgerMenu = document.querySelector('.burger__menu');
+    
+    burgerMenu.addEventListener('click', () => {
+        burgerMenu.classList.toggle('active');
         header.classList.toggle('scrolled-up');
     });
-})
+
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    document.getElementById(prefersDarkScheme ? 'dark' : 'light').checked = true;
+});
