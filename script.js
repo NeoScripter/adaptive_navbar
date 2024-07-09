c179aaff19fa616e1755293c0ea64b54dc444cfc
Ilya <sange0337@gmail.com>
2024-07-09 14:14:54 +0800

    finished the layout

--- script.js
+++ script.js
@@ -1,40 +1,7 @@
-const themeswitch = document.querySelector(".navbar__themeswitch");
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-const currentTheme = localStorage.getItem("theme");
-if (currentTheme == "dark") {
-  document.body.classList.toggle("dark-theme");
-} else if (currentTheme == "light") {
-  document.body.classList.toggle("light-theme");
-}
-
-themeswitch.addEventListener("click", function () {
-  if (prefersDarkScheme.matches) {
-    document.body.classList.toggle("light-theme");
-    var theme = document.body.classList.contains("light-theme")
-      ? "light"
-      : "dark";
-  } else {
-    document.body.classList.toggle("dark-theme");
-    var theme = document.body.classList.contains("dark-theme")
-      ? "dark"
-      : "light";
-  }
-  localStorage.setItem("theme", theme);
-});
-
-const navicon = document.querySelector(".navbar__navicon");
-const navbar = document.querySelector(".navbar");
-const body = document.querySelector("body");
-
-navicon.onclick = function (e) {
-  navbar.classList.toggle("active")
-  body.classList.toggle("h-0")
-};
-
-const logotype = document.querySelector(".navbar__logo");
-
-themeswitch.onclick = function (e) {
-  themeswitch.classList.toggle("active")
-  logotype.classList.toggle("active")
-};
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector('.header__navbar-wrapper');
+    document.querySelector('.burger__menu').addEventListener('click', function() {
+        this.classList.toggle('active');
+        header.classList.toggle('scrolled-up');
+    });
+})
